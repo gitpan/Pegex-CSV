f901628f7f26d5aa9a1d554d08929c3b7bb15334
Ingy döt Net <ingy@cpan.org>
2014-10-11 16:58:32 +0000

    Import of INGY/Pegex-CSV-0.0.4 from CPAN.
    gitpan-cpan-distribution: Pegex-CSV
    gitpan-cpan-version:      0.0.4
    gitpan-cpan-path:         INGY/Pegex-CSV-0.0.4.tar.gz
    gitpan-cpan-author:       INGY
    gitpan-cpan-maturity:     released

--- inc/Pegex.pm
+++ inc/Pegex.pm
@@ -8,16 +8,12 @@ use Exporter 'import';
 our @EXPORT = 'pegex';
 
 sub pegex {
-    my ($grammar, $receiver, $debug) = @_;
+    my ($grammar, $receiver) = @_;
     die "Argument 'grammar' required in function 'pegex'"
         unless $grammar;
     if (not ref $grammar or $grammar->isa('Pegex::Input')) {
         require Pegex::Grammar;
         $grammar = Pegex::Grammar->new(text => $grammar),
-    }
-    if ($receiver eq '1') {
-        $debug = 1;
-        $receiver = undef;
     }
     if (not defined $receiver) {
         require Pegex::Tree::Wrap;
@@ -27,11 +23,9 @@ sub pegex {
         eval "require $receiver; 1";
         $receiver = $receiver->new;
     }
-    $debug ||= 0;
     return Pegex::Parser->new(
         grammar => $grammar,
         receiver => $receiver,
-        debug => $debug,
     );
 }
 
